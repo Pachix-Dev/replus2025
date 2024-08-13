ae662f733ac8b5414c9b7e060b3b9b0536919544
Jesus Zermeño <josezerrod@hotmail.com>
2024-08-12 21:34:12 -0600

    Update file links in Menu and Home components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -71,7 +71,7 @@ export function Home() {
                 ? '/files/RE+MEX_2025_FS_ENG_AC_24-04-2024.pdf'
                 : '/files/RE+MEX_2025_FS_ESP_AC_24-04-2024.pdf'
             } target='_blank' className='text-decoration-none'><div className='btn-item'> {t("menu.submenu_2.option_6")}  </div></a>
-            <a href='/files/RE+MEX_floorplant_2025_v6_02_08_25.pdf'
+            <a href='/files/RE+MEX_floorplant_2025_12_08_25.pdf'
               target='_blank' className='text-decoration-none'><div className='btn-item'> {t("menu.submenu_2.option_8")} 2025 </div></a>
             <a href='/files/REMEX_2025_SpaceAgreement_ED_160524.pdf'
               target='_blank' className='text-decoration-none'><div className='btn-item'>  {t("menu.submenu_2.option_7")} </div></a>
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -83,7 +83,7 @@ export function Menu() {
                   </NavDropdown.Item>
                   
                   <NavDropdown.Item
-                    href='/files/RE+MEX_floorplant_2025_v6_02_08_25.pdf'
+                    href='/files/RE+MEX_floorplant_2025_12_08_25.pdf'
                     target='_blank'
                     onClick={handleClose}
                   >
